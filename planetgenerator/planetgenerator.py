4760857f227c278f4dd667c53f0ba6b9d172a53a
Hoots <ajvanhout@gmail.com>
2019-04-19 09:41:32 -0500

    continued work on first draft, finished core UPP profile

--- planetgenerator/planetgenerator.py
+++ planetgenerator/planetgenerator.py
@@ -1,5 +1,7 @@
 import rolls
 
+# rolled_temperature = rolls.roll_temperature(rolled_atmosphere)
+
 class TravellerPlanet(object):
     def __init__(self, size, atmosphere, hydrographics, population, government, law_level, tech_level, starport):
         self.size = size
@@ -15,28 +17,31 @@ def roll_planet():
     rolled_size = rolls.roll_size()
     rolled_atmosphere = rolls.roll_atmosphere(rolled_size)
     rolled_hydrographics = rolls.roll_hydrographics(rolled_size, rolled_atmosphere)
-    rolled_temperature = rolls.roll_temperature(rolled_atmosphere)
     rolled_population = rolls.roll_population(rolled_size, rolled_atmosphere)
     rolled_government = rolls.roll_government(rolled_population)
-    rolled_law_level = 3
-    rolled_tech_level = 9
-    rolled_starport = "A"
-    
+    rolled_law_level = rolls.roll_law_level(rolled_government)
+    rolled_starport = rolls.starport_calc(rolled_population)
+    rolled_tech_level = rolls.roll_tech_level(rolled_population, rolled_atmosphere)
+    rolled_temperature = rolls.roll_temperature(rolled_atmosphere)
+
     results = {"size": rolled_size, "atmosphere": rolled_atmosphere, "hydrographics": rolled_hydrographics,
                "temperature": rolled_temperature, "population": rolled_population, "government": rolled_government,
-               "law_level": rolled_law_level, "tech_level": rolled_tech_level, "starport": rolled_starport
+               "law_level": rolled_law_level, "starport": rolled_starport, "tech_level": rolled_tech_level
                }
     
     return results
 
-rr = roll_planet()
+def print_new_planets():
+    
+    rr = roll_planet()
 
-newplanet = TravellerPlanet(rr['size'], rr['atmosphere'], rr['hydrographics'], rr['temperature'], rr['population'],
-rr['government'], rr['law_level', rr['tech_level'], rr['starport'] )
-printout = f"""
-Size: {newplanet.size}, Atmosphere: {newplanet.atmosphere}, Hydrographics: {newplanet.hydrographics}, Temperature: {newplanet.temperature}, 
-Population {newplanet.population}, Government {newplanet.government}, Law Level {newplanet.law_level}, Tech Level: {newplanet.tech_level}
-Starport Class: {newplanet.starport}
-"""
+    newplanet = TravellerPlanet(rr['size'], rr['atmosphere'], rr['hydrographics'], rr['population'],
+    rr['government'], rr['law_level'], rr['tech_level'], rr['starport'] )
 
-print(printout)
+    printout = f"{newplanet.size},{newplanet.atmosphere},{newplanet.hydrographics},{newplanet.population},{newplanet.government},{newplanet.law_level},{newplanet.starport},{newplanet.tech_level}"
+    print (printout)
+
+
+for x in range (500):
+    print_new_planets()
+
--- planetgenerator/rolls.py
+++ planetgenerator/rolls.py
@@ -16,18 +16,44 @@ def roll_atmosphere(planet_size):
     if (planet_size == 0 or planet_size == 1):
         return 0
     elif (unusual_chance == 0 or unusual_chance == 100):
-        return 15
+        return "F"
     else:
         number = (sum_two_dee_six() - 7 + planet_size)
         if number < 0:
             return 0
         elif number > 15:
-            return 15
+            return "F"
+        elif (number > 9):
+            if (number == 10):
+                return "A"
+            elif (number == 11):
+                return "B"
+            elif (number == 12):
+                return "C"
+            elif (number == 13):
+                return "D"
+            elif (number == 14):
+                return "E"
+            elif (number == 15):
+                return "F"
         else:
             return number
+        
 
 def roll_hydrographics(planet_size, planet_atmosphere):
-    
+    if (planet_atmosphere == "A"):
+        planet_atmosphere = 10
+    if (planet_atmosphere == "B"):
+        planet_atmosphere = 11
+    if planet_atmosphere == "C":
+        planet_atmosphere = 12
+    if planet_atmosphere == "D":
+        planet_atmosphere = 13
+    if planet_atmosphere == "E":
+        planet_atmosphere = 14
+    if planet_atmosphere == "F":
+        planet_atmosphere = 15
+
     if (planet_size == 0 or planet_size == 1):
         return 0
     elif (planet_atmosphere == 0 or planet_atmosphere == 1 or planet_atmosphere == 10 or planet_atmosphere == 11 or planet_atmosphere == 12):
@@ -35,7 +61,7 @@ def roll_hydrographics(planet_size, planet_atmosphere):
         if number < 0:
             return 0
         elif number > 10:
-            return 10
+            return "A"
         else:
             return number
     else:
@@ -49,16 +75,16 @@ def roll_hydrographics(planet_size, planet_atmosphere):
 
 def roll_temperature(planet_atmosphere):
     if (planet_atmosphere == 0 or planet_atmosphere == 1):
-        return "Wildly Variable"
+        return "Variable"
     elif (planet_atmosphere == 2 or planet_atmosphere == 3):
         return sum_two_dee_six() - 2
-    elif (planet_atmosphere == 4 or planet_atmosphere == 5 or planet_atmosphere == 14):
+    elif (planet_atmosphere == 4 or planet_atmosphere == 5 or planet_atmosphere == "E"):
         return sum_two_dee_six() - 1
     elif (planet_atmosphere == 6 or planet_atmosphere == 7):
         return sum_two_dee_six()
     elif (planet_atmosphere == 8 or planet_atmosphere == 9):
         return sum_two_dee_six() + 1
-    elif (planet_atmosphere == 10 or planet_atmosphere == 13 or planet_atmosphere == 15):
+    elif (planet_atmosphere == "A" or planet_atmosphere == "D" or planet_atmosphere == "F"):
         return sum_two_dee_six() + 2
     else:
         return sum_two_dee_six() + 6
@@ -73,8 +99,82 @@ def roll_government(planet_population):
     if (planet_population == 0):
         return 0
     else:
-        govresult = sum_two_dee_six() + planet_population - 7
+        govresult = sum_two_dee_six() + planet_population - 4
         if govresult < 0:
             return 0
         else:
             return govresult
+
+def roll_law_level(planet_government):
+    if (planet_government == 0):
+        return 0
+    else:
+        law_result = sum_two_dee_six() + planet_government - 7
+        if law_result < 0:
+            return 0
+        elif law_result > 9:
+            return 9
+        else:
+            return law_result
+
+def roll_startport(planet_population):
+    if planet_population <= 2:
+        return sum_two_dee_six() - 2
+    elif (planet_population <= 4 and planet_population > 2):
+        return sum_two_dee_six() - 1
+    elif (planet_population >= 8 and planet_population < 10):
+        return sum_two_dee_six() + 1
+    else:
+        return sum_two_dee_six()
+
+def starport_calc(planet_population):
+    
+    result = roll_startport(planet_population)
+    if (result <= 2):
+        return "X"
+    elif (result == 3 or result == 4):
+        return "E"
+    elif (result == 5 or result == 6):
+        return "D"
+    elif (result == 7 or result == 8):
+        return "C"
+    elif (result == 9 or result == 10):
+        return "B"
+    else:
+        return "A"
+
+def roll_tech_level(rolled_population, rolled_atmosphere):
+    
+    if (rolled_population == 0):
+        return 0
+    elif (rolled_population < 6):
+        result = one_dee_six()
+        if (rolled_atmosphere == 0 or rolled_atmosphere == 1):
+            return 8
+        elif (rolled_atmosphere == 2 or rolled_atmosphere == 3):
+            if result > 5:
+                return result
+            else:
+                return 5
+        elif (rolled_atmosphere == 4 or rolled_atmosphere == 7 or rolled_atmosphere == 9):
+            if (result > 3):
+                return result
+            else:
+                return 3
+        elif (rolled_atmosphere == "A"):
+            return 8
+        elif (rolled_atmosphere == "B"):
+            return 9
+        elif (rolled_atmosphere == "C"):
+            return "A"
+        elif (rolled_atmosphere == "D" or rolled_atmosphere == "E"):
+            if (result > 5):
+                return result
+            else:
+                return 5
+        elif (rolled_atmosphere == "F"):
+            return 8
+        else:
+            return result
+    elif (rolled_population == 6 or rolled_population == 7):
+        return "A"
