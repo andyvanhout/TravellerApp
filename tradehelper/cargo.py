cff56d98a27c7c0d64e2d3626e1fbaa87476cdee
Hoots <ajvanhout@gmail.com>
2019-04-27 23:18:47 -0500

    worked on supplier.py and finished some cargo calculations

--- tradehelper/cargo.py
+++ tradehelper/cargo.py
@@ -1,19 +1,21 @@
 import traffic
 
-def major_cargo(steward_skill, population_modifier, starport_modifier, tech_level, amber_modifier, red_modifier):
-    dice_modifier = steward_skill + population_modifier + starport_modifier + tech_level + amber_modifier + red_modifier - 4 #minus four for major cargo
+def major_cargo(skill_effect, population_modifier, starport_modifier, tech_level, amber_modifier, red_modifier):
+    dice_modifier = skill_effect + population_modifier + starport_modifier + tech_level + amber_modifier + red_modifier - 4 #minus four for major cargo
     #print(f'Dice Modifier for High Passage: {dice_modifier}')
     major_lots = traffic.freight_traffic(dice_modifier)
     print(f'Number of Major Lots: {major_lots}')
 
-def minor_cargo(steward_skill, population_modifier, starport_modifier, tech_level, amber_modifier, red_modifier):
-    dice_modifier = steward_skill + population_modifier + starport_modifier + tech_level + amber_modifier + red_modifier
+def minor_cargo(skill_effect, population_modifier, starport_modifier, tech_level, amber_modifier, red_modifier):
+    dice_modifier = skill_effect + population_modifier + starport_modifier + tech_level + amber_modifier + red_modifier
     #print(f'Dice Modifier for Medium Passage: {dice_modifier}')
     minor_lots = traffic.freight_traffic(dice_modifier)
     print(f'Number of Minor Lots: {minor_lots}')
 
-def incidental_cargo(steward_skill, population_modifier, starport_modifier, tech_level, amber_modifier, red_modifier):
-    dice_modifier = steward_skill + population_modifier + starport_modifier + tech_level + amber_modifier + red_modifier + 2 #plus two for incidental cargo
+def incidental_cargo(skill_effect, population_modifier, starport_modifier, tech_level, amber_modifier, red_modifier):
+    dice_modifier = skill_effect + population_modifier + starport_modifier + tech_level + amber_modifier + red_modifier + 2 #plus two for incidental cargo
     #print(f'Dice Modifier for Basic Passage: {dice_modifier}')
     incidental_lots = traffic.freight_traffic(dice_modifier)
     print(f'Number of Incidental Lots: {incidental_lots}')
+    
+#maybe implement mail some day
---
+++ tradehelper/suppliers.py
@@ -0,0 +1,28 @@
+import random
+
+def roll_dee_six(number):
+    result = []
+    for i in range(number):
+        result.append(random.randint(1,6))
+    return result
+
+def sum_roll_dee_six(number):
+    dice_list = roll_dee_six(number)
+    return sum(dice_list)
+
+def combine_dee_six(number):
+    combined_dice = roll_dee_six(number)
+    combined_string = ''.join(map(str, combined_dice))
+    return int(combined_string)
+
+def goods_available():
+    random_goods = sum_roll_dee_six(1)
+    print(f'This supplier has {random_goods} available. They are:')
+    for i in range(random_goods):
+        random_item = combine_dee_six(2)
+        print(f'Item {random_item}')
+
+goods_available()
+
+#bedtime thoughts... I could return a string for goods_available, and then set up a data dict with each D66 roll for each good,
+#which would allow my to return what actual good is returned.
