7b338c5abd5187e00cb7d233ebae56089d0a731c
Van Hout <AVanHout@henryscheinvet.com>
2019-05-14 17:04:03 -0500

    more trade codes work

--- tradehelper/suppliers.py
+++ tradehelper/suppliers.py
@@ -1,4 +1,5 @@
 import random
+import modules.trade_goods as tg
 
 def roll_dee_six(number):
     result = []
@@ -16,6 +17,8 @@ def combine_dee_six(number):
     return int(combined_string)
 
 def goods_available():
+    trade_codes = input("Input Trade Codes: ")
+    print(trade_codes)
     random_goods = sum_roll_dee_six(1)
     print(f'This supplier has {random_goods} available. They are:')
     for x in range(random_goods):
@@ -23,6 +26,3 @@ def goods_available():
         print(f'Item {random_item}')
 
 goods_available()
-
-#bedtime thoughts... I could return a string for goods_available, and then set up a dict with each D66 roll for each good,
-#which would allow me to return what actual good is returned.
